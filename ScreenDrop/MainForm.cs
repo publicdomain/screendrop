874b6674f77adcaaa07563f3a9bdad4d359a84eb
Victor <publicdomainvic@gmail.com>
2022-07-08 13:01:33 -0400

    Add OnFreeReleasesPublicDomainIsToolStripMenuItemClick()

--- ScreenDrop/MainForm.cs
+++ ScreenDrop/MainForm.cs
@@ -191,7 +191,8 @@ namespace ScreenDrop
         /// <param name="e">Event arguments.</param>
         private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open our website
+            Process.Start("https://publicdomain.is");
         }
 
         /// <summary>
